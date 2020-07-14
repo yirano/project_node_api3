1e7256a257e27aeffbe7f99a9c645644be9d6351
yirano <yirano@protonmail.ch>
2020-07-14 00:26:56 -0400

    User can create a new user and retrieve all user data

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code away!
+const express = require('express')
+
+const userRouter = require('./users/userRouter')
+const postRouter = require('./posts/postRouter')
+
+const server = express()
+const PORT = 4000
+
+server.use(express.json())
+
+server.use('/users', userRouter)
+server.use(postRouter)
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,34 +1,72 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const userDB = require('./userDb')
+const { getById } = require('./userDb')
 
-router.post('/', (req, res) => {
+const router = express.Router()
+
+router.post('/', async (req, res) => {
   // do your magic!
-});
+  try {
+    if (req.body.name) {
+      const postUser = await userDB.insert(req.body)
+      if (postUser) {
+        res.status(201).json({ data: postUser })
+      } else {
+        res.status(400).json({ message: 'User could not be created.' })
+      }
+    } else {
+      res.status(400).json({ message: 'Please make sure your input is valid' })
+    }
+  } catch (error) {
+    res.status(500).json({ errorMessage: 'Something went wrong' })
+  }
+})
 
 router.post('/:id/posts', (req, res) => {
   // do your magic!
-});
+})
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // do your magic!
-});
+  try {
+    const users = await userDB.get()
+    if (users) {
+      res.status(200).json({ data: users })
+    } else {
+      res.status(404).json({ message: 'Could not retrieve users' })
+    }
+  } catch {
+    res.status(500).json({ errorMessage: 'Something went wrong.' })
+  }
 
-router.get('/:id', (req, res) => {
+})
+
+router.get('/:id', async (req, res) => {
   // do your magic!
-});
+  try {
+    const user = await userDB.getById(req.params.id)
+    if (user) {
+      res.status(200).json({ data: user })
+    } else {
+      res.status(404).json({ message: 'User by that ID could not be found' })
+    }
+  } catch {
+    res.status(500).json({ errorMessage: 'Something went wrong.' })
+  }
+})
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
-});
+})
 
 router.delete('/:id', (req, res) => {
   // do your magic!
-});
+})
 
 router.put('/:id', (req, res) => {
   // do your magic!
-});
+})
 
 //custom middleware
 
@@ -44,4 +82,4 @@ function validatePost(req, res, next) {
   // do your magic!
 }
 
-module.exports = router;
+module.exports = router
